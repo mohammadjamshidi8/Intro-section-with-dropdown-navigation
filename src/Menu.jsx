9efe9810d94315bb8104743bfdc14a0704024dea
mohamad jamshidi <160650161+mohammadjamshidi8@users.noreply.github.com>
2024-04-20 16:23:05 -0700

    style: create mobile menu

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,7 +1,44 @@
+// const phoneSubMenuWrapper = document.querySelector('#mobile-sub-menu')
+// const phoneMenuItem = document.querySelector('#mobile-menu')
+
+// console.log(phoneMenuItem);
+
+// phoneMenuItem.addEventListener('click',() => {
+//   console.log('true');
+// })
+
 export default function Menu() {
+  const firstMobileMenuItem = () => {
+    const phoneSubMenuWrapper = document.querySelector("#mobile-sub-menu");
+    const arrowIcon = document.querySelector('#arrow-icon')
+
+    phoneSubMenuWrapper.classList.toggle('h-0')
+    phoneSubMenuWrapper.classList.toggle('opacity-0')
+    arrowIcon.classList.toggle('rotate-180')
+  };
+
+  const secondMobileMenuItem = () => {
+    const phoneSubMenuWrapper2 = document.querySelector("#mobile-sub-menu-2");
+    const arrowIcon2 = document.querySelector('#arrow-icon-2')
+
+    phoneSubMenuWrapper2.classList.toggle('h-0')
+    phoneSubMenuWrapper2.classList.toggle('opacity-0')
+    arrowIcon2.classList.toggle('rotate-180')
+  }
+
+  const closeFunc = (e) => {
+    const closeIcon = document.querySelector('#close-icon')
+    const mobileMenuContainer = document.querySelector('#mobile-menu-container')
+
+    mobileMenuContainer.classList.toggle('right-[-4000px]')
+    mobileMenuContainer.classList.toggle('right-0')
+    mobileMenuContainer.classList.toggle('opacity-0')
+    
+  }
+
   return (
     <>
-      <div className="w-full flex justify-between items-center mt-6 px-3 lg:px-8 bg-rose-300">
+      <div className="w-full flex justify-between items-center pt-6 px-3 lg:px-8">
         <div className="flex items-center gap-x-10">
           <div>
             <img src="./public/images/logo.svg" alt="" />
@@ -14,41 +51,41 @@ export default function Menu() {
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                   stroke="currentColor"
-                  class="w-4 h-4 mt-1 group-hover:rotate-180"
+                  className="w-4 h-4 mt-1 group-hover:rotate-180"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="m19.5 8.25-7.5 7.5-7.5-7.5"
                   />
                 </svg>
               </a>
               <ul className="invisible opacity-0 absolute top-8 w-max group-hover:visible flex group-hover:opacity-100  flex-col gap-y-4 bg-white px-8 rounded-md py-4 transition-all shadow-lg">
                 <li>
-                    <a href="#" className="flex items-center gap-x-4">
+                  <a href="#" className="flex items-center gap-x-4">
                     <img src="./public/images/icon-todo.svg" alt="" />
                     <span className="capitalize">todo list</span>
-                    </a>
+                  </a>
                 </li>
                 <li>
-                <a href="#" className="flex items-center gap-x-4">
+                  <a href="#" className="flex items-center gap-x-4">
                     <img src="./public/images/icon-calendar.svg" alt="" />
                     <span className="capitalize">calendar</span>
-                    </a>
+                  </a>
                 </li>
                 <li>
-                <a href="#" className="flex items-center gap-x-4">
+                  <a href="#" className="flex items-center gap-x-4">
                     <img src="./public/images/icon-reminders.svg" alt="" />
                     <span className="capitalize">reminders</span>
-                    </a>
+                  </a>
                 </li>
                 <li>
-                <a href="#" className="flex items-center gap-x-4">
+                  <a href="#" className="flex items-center gap-x-4">
                     <img src="./public/images/icon-planning.svg" alt="" />
                     <span className="capitalize">planning</span>
-                    </a>
+                  </a>
                 </li>
               </ul>
             </li>
@@ -59,32 +96,32 @@ export default function Menu() {
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                   stroke="currentColor"
-                  class="w-4 h-4 mt-1 group-hover:rotate-180"
+                  className="w-4 h-4 mt-1 group-hover:rotate-180"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="m19.5 8.25-7.5 7.5-7.5-7.5"
                   />
                 </svg>
               </a>
               <ul className="invisible opacity-0 absolute top-8 w-max group-hover:visible flex group-hover:opacity-100  flex-col gap-y-4 bg-white pl-3 pr-5 rounded-md py-4 transition-all shadow-lg">
                 <li>
-                    <a href="#" className="flex items-center gap-x-4">
+                  <a href="#" className="flex items-center gap-x-4">
                     <span className="capitalize">history</span>
-                    </a>
+                  </a>
                 </li>
                 <li>
-                <a href="#" className="flex items-center gap-x-4">
+                  <a href="#" className="flex items-center gap-x-4">
                     <span className="capitalize">our team</span>
-                    </a>
+                  </a>
                 </li>
                 <li>
-                <a href="#" className="flex items-center gap-x-4">
+                  <a href="#" className="flex items-center gap-x-4">
                     <span className="capitalize">blog</span>
-                    </a>
+                  </a>
                 </li>
               </ul>
             </li>
@@ -98,8 +135,9 @@ export default function Menu() {
         </div>
         <div className="flex gap-x-4 items-center">
           <img
+            onClick={closeFunc}
             src="./public/images/icon-menu.svg"
-            className="lg:hidden"
+            className="lg:hidden cursor-pointer"
             alt=""
           />
           <button className="hidden lg:block capitalize">login</button>
@@ -107,6 +145,131 @@ export default function Menu() {
             register
           </button>
         </div>
+
+        {/* mobile menu start */}
+        <div id="mobile-menu-container" className="transition-all duration-300 lg:hidden opacity-0 fixed p-5 top-0 bottom-0 right-[-4000px] w-[55%] h-full bg-white flex flex-col gap-y-14 z-50">
+          <div className="w-full flex justify-end">
+            <svg
+              onClick={closeFunc}
+              id="close-icon"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-10 h-10 cursor-pointer"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18 18 6M6 6l12 12"
+              />
+            </svg>
+          </div>
+
+          <div className="capitalize">
+            <ul className="flex flex-col gap-y-4 pl-5">
+              <li onClick={firstMobileMenuItem} >
+                <a href="#" className="flex gap-x-1 items-center">
+                  <span className="text-2xl">features</span>
+                  <svg
+                  id="arrow-icon"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-5 h-5 mt-1"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                    />
+                  </svg>
+                </a>
+                <ul
+                  id="mobile-sub-menu"
+                  className="flex flex-col gap-y-3 mt-2 pl-10 overflow-hidden h-0 opacity-0 transition-all duration-500 text-lg"
+                >
+                  <li className="capitalize">
+                    <a href="#" className="flex items-center gap-x-3">
+                      <img src="./public/images/icon-todo.svg" alt="" />
+                      <span>todo list</span>
+                    </a>
+                  </li>
+                  <li className="capitalize">
+                    <a href="#" className="flex items-center gap-x-3">
+                      <img src="./public/images/icon-calendar.svg" alt="" />
+                      <span>calendar</span>
+                    </a>
+                  </li>
+                  <li className="capitalize">
+                    <a href="#" className="flex items-center gap-x-3">
+                      <img src="./public/images/icon-reminders.svg" alt="" />
+                      <span>reminders</span>
+                    </a>
+                  </li>
+                  <li className="capitalize">
+                    <a href="#" className="flex items-center gap-x-3">
+                      <img src="./public/images/icon-planning.svg" alt="" />
+                      <span>planning</span>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li onClick={secondMobileMenuItem}>
+                <a href="#" className="flex gap-x-1 items-center">
+                  <span className="text-2xl">company</span>
+                  <svg
+                  id="arrow-icon-2"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-5 h-5 mt-1"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                    />
+                  </svg>
+                </a>
+                <ul
+                  id="mobile-sub-menu-2"
+                  className="flex flex-col gap-y-3 mt-2 pl-10 overflow-hidden h-0 opacity-0 transition-all duration-500 text-lg"
+                >
+                  <li className="capitalize">
+                    <a href="#" className="flex items-center gap-x-3">
+                      <span>history</span>
+                    </a>
+                  </li>
+                  <li className="capitalize">
+                    <a href="#" className="flex items-center gap-x-3">
+                      <span>our teams</span>
+                    </a>
+                  </li>
+                  <li className="capitalize">
+                    <a href="#" className="flex items-center gap-x-3">
+                      <span>blog</span>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li><a className="text-2xl" href="#">Careers</a></li>
+              <li><a className="text-2xl" href="#">about</a></li>
+            </ul>
+          </div>
+
+          <div className="flex flex-col gap-y-5">
+            <button className="w-10/12 text-xl text-center mx-auto py-3 rounded-2xl">Login</button>
+            <button className="w-10/12 text-xl text-center mx-auto py-3 rounded-2xl border-[1px] border-mediumGrey">Register</button>
+          </div>
+        </div>
+        {/* mobile menu ends */}
+
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,11 @@ export default {
         almostWhite : 'hsl(0, 0%, 98%)',
         mediumGrey : 'hsl(0, 0%, 41%)',
         almostgray : 'hsl(0, 0%, 8%)'
-      }
+      },
+      backgroundImage: {
+        'desktop': "url('./public/images/image-hero-desktop.png')",
+        'mobile': "url('./public/images/image-hero-mobile.png')",
+      },
     },
   },
   plugins: [],
